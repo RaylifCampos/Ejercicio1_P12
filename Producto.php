6c6a869df486dc4f178038e062743ab3244442fb
unknown <andre.campos@ucsm.edu.pe>
2021-11-30 16:26:14 -0500

    Ejercicio 1: Productos con descripciones

---
+++ Producto.php
@@ -0,0 +1,134 @@
+<?PHP
+if ($_REQUEST['cod']==1){
+    echo '  <div class="modal-header text-center">
+                <h5 class="modal-title  w-100" id="modalTitle"><strong> Polo Manga Corta Oversize</strong></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-level="Close"> </button>
+            </div>
+            <div class="modal-body">
+                <div class="container">
+
+			    <img class="card-img-top border" src="Productos/produ1.jpg" alt="..." "/>
+                            <hr>
+                            <small>Código: L340-15IRH</small>
+                            <h5><strong>LAPTOP LENOVO L340-15IRH</strong></h5>
+                            <br>
+                            <br>
+                            <p><strong>Color:</strong><br>Negro</p>
+                            <br>
+                            <strong>Unidades en stock:</strong> 7<br>
+                            <strong>Características Destacadas</strong>
+                            <p><strong>Marca:</strong> Lenovo<br>
+                            <strong>Modelo:</strong> L340-15IRH<br>
+                            <strong>Producto:</strong> Laptop<br>
+                            <strong>Género:</strong> Core i5 - 9th Gen<br>
+                            <strong>Tarjeta de video:</strong> GTX 1650
+			    <strong>Memoria RAM:</strong> 8GB DDR4<br>
+                            <strong>Almacenamiento:</strong> 512GB(SSD)<br>
+                            <strong>Pantalla:</strong> 15.6 FHD<br>
+                            </p>
+                            <div >S/ 4109.00</div>
+                            <hr>
+                </div>
+            </div>
+            <div class="modal-footer">              
+            </div>';
+}
+if ($_REQUEST['cod']==2){
+    echo '  <div class="modal-header text-center">
+                <h5 class="modal-title  w-100" id="modalTitle"><strong>LAPTOP ASUS EXPERTBOOK P2451FA-EK1442</strong></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-level="Close"> </button>
+            </div>
+            <div class="modal-body">
+                <div class="container">
+                            <img class="card-img-top border" src="Productos/produ2.jpg" alt="..." />
+                            <hr>
+                            <small>Código: P2451FA-EK1442</small>
+                            <h5><strong>LAPTOP ASUS EXPERTBOOK P2451FA-EK1442</strong></h5>
+                            <br>
+                            <br>
+                            <p><strong>Color:</strong><br>Negro</p>
+                            <br>
+                            <strong>Unidades en stock:</strong> 0<br>
+                            <strong>Características Destacadas</strong>
+                            <p><strong>Marca:</strong> Asus<br>
+                            <strong>Modelo:</strong> L340-15IRH<br>
+                            <strong>Producto:</strong> Laptop<br>
+                            <strong>Género:</strong> Core i7 - 10th Gen<br>
+                            <strong>Tarjeta de video:</strong> Intel UHD Graphics<br>
+			    <strong>Memoria RAM:</strong> 8GB DDR4<br>
+                            <strong>Almacenamiento:</strong> 1TB (HDD)<br>
+                            <strong>Pantalla:</strong> 14 FHD<br>
+                            </p>
+                            <p><h5>S/ 3998.00</h5></p>                            
+                            <hr>
+                </div>
+            </div>
+            <div class="modal-footer">              
+            </div>';
+}
+if ($_REQUEST['cod']==3){
+    echo '  <div class="modal-header text-center">
+                <h5 class="modal-title  w-100" id="modalTitle"><strong>LAPTOP ACER ASPIRE 3 A315-57G-53R9</strong></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-level="Close"> </button>
+            </div>
+            <div class="modal-body">
+                <div class="container">
+                            <img class="card-img-top border" src="Productos/produ3.jpg" alt="..." />
+                            <hr>
+                            <smallCódigo: A315-57G-53R9</small>
+                            <h5><strong>LAPTOP ACER ASPIRE 3 A315-57G-53R9</strong></h5>
+                            <br>
+                            <br>
+                            <p><strong>Color:</strong><br>Rojo<br>Blanco<br>Gris</p>
+                            <br>
+                            <strong>Unidades Disponibles:</strong> 0 (Agotado)<br>
+
+                            <strong>Características Destacadas</strong>
+                            <p><strong>Marca:</strong> Acer<br>
+                            <strong>Modelo:</strong> A315-57G-53R9<br>
+                            <strong>Producto:</strong> Laptop<br>
+                            <strong>Género:</strong> Core i5 - 10th Gen<br>
+                            <strong>Tarjeta de video:</strong> MX 330<br>
+			    <strong>Memoria RAM:</strong> 8GB DDR4<br>
+                            <strong>Almacenamiento:</strong> 256GB(SSD)<br>
+                            <strong>Pantalla:</strong> 15.6 FHD<br>
+                            <p><h5>S/ 3000.00 </h5></p>
+                            
+                            <hr>
+                </div>
+            </div>
+            <div class="modal-footer">              
+            </div>';
+}
+if ($_REQUEST['cod']==4){
+    echo '  <div class="modal-header text-center">
+                <h5 class="modal-title  w-100" id="modalTitle"><strong>MONITOR HP X27 27 FHD GAMING 2V6B2AA</strong></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-level="Close"> </button>
+            </div>
+            <div class="modal-body">
+                <div class="container">
+                            <img class="card-img-top border" src="Productos/produ4.jpg" alt="..." />
+                            <hr>
+                            <small>Código:  2V6B2AA</small>
+                            <h5><strong>MONITOR HP X27 27 FHD GAMING 2V6B2AA</strong></h5>
+                            <br>
+                            <br>
+                            <p><strong>Color:</strong><br>Negro<br>Gris</p>
+                            <br>
+
+                            <strong>Características Destacadas</strong>
+                            <p><strong>Marca:</strong> HP<br>
+                            <strong>Modelo:</strong> X27<br>
+                            <strong>Producto :</strong>  Monitores<br>
+                            <strong>Tiempo de respuesta:</strong> 1MS<br>
+                            <strong>Frecuencia :</strong> 165HZ<br>
+                            <strong>Pantalla:</strong> 27 FHD<br>
+                            </p>
+                            <div>S/ 1225.00</div>
+                            <hr>
+                </div>
+            </div>
+            <div class="modal-footer">              
+            </div>';
+}
+?>
